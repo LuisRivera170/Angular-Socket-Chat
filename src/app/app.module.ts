f864b44783003d4138c5253553dd7354cc37be3e
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-10-28 14:46:51 -0600

    Chat component creation, first routing configuration, ng-bootstrap installation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+	AppRoutingModule,
+	FormsModule,
+	NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
